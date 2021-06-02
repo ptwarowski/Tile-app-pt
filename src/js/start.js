cc728c20f802c51145c1eb073da397687230d818
Przemek Twarowski <przemektwarowski@gmail.com>
2021-06-02 23:39:55 +0200

    Tile App v1.1 to deploy

--- src/js/start.js
+++ src/js/start.js
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 export const Start = () => {
     return (
-    <div id="startBox">
-        <section className='middleSection'>
-            <h1><a href='app.html'>Start Planning</a></h1>
-        </section>
-    </div>
+        <div id="startBox">
+            <section className='middleSection'>
+            
+            <h1><Link to="/app.html">
+              Start Planning
+            </Link></h1>
+            </section>
+        </div>
+    
     )
 }
